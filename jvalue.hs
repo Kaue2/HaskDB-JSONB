d49be1ebb9f70acf6c8d15c02c329c649b40b7de
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-06-07 02:29:15 -0300

    add: adicionar .gitignore e formatar codigo

--- jvalue.hs
+++ jvalue.hs
@@ -1,4 +1,18 @@
-import Data.Text
-import qualified Data.Map as Map
+{-# LANGUAGE OverloadedStrings #-}
 
-data Jvalue = Value Text | List [Jvalue] | Object (Map.Map Text Jvalue) | Number Double | Boolean Bool | Null
+import Data.Map qualified as Map
+import Data.Text
+
+data Jvalue
+  = Value Text
+  | List [Jvalue]
+  | Object (Map.Map Text Jvalue)
+  | Number Double
+  | Boolean Bool
+  | Null
+  deriving (Show)
+
+main :: IO ()
+main = do
+  let doc = Object (Map.fromList [("Nome", Object (Map.fromList [("Nome", Value "exemplo"), ("Idade", Number 15)]))])
+  print doc
